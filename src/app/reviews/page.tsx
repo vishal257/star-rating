ad964a09d2e116b43cd96e49907de8a3f5dcc2b8
Vishal <vishal2573003@gmail.com>
2024-04-12 22:33:48 +0530

    used router instead of useSearchParams

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -3,6 +3,7 @@
 import ReviewCard from "@/pages/Components/ReviewCard";
 import localFont from "next/font/local";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 const newFont = localFont({
   src: "../../styles/mangueiraalt-alt.otf",
@@ -10,10 +11,13 @@ const newFont = localFont({
 });
 
 const Reviews = () => {
-  const params = useSearchParams()?.get('stars');
-  const paramsText = useSearchParams()?.get('textValue');
-  const stars = params || "";
-  const review = paramsText || "";
+  const router = useRouter();
+  const params = router.query.stars;
+  const paramsText = router.query.textValue;
+  // const params = useSearchParams()?.get('stars');
+  // const paramsText = useSearchParams()?.get('textValue');
+  const stars = Array.isArray(params) ? params[0] : params|| "";
+  const review = Array.isArray(paramsText) ? paramsText[0] : paramsText || "";
 
   return (
     <div className={`mx-auto xxsm:w-4/5 w-11/12 grid items-center h-dvh ${newFont.variable} font-sans`}>
--- src/pages/Components/ReviewCard.tsx
+++ src/pages/Components/ReviewCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState } from "react";
+// import { useState } from "react";
 
 const ReviewCard = ({ stars, review }: { stars: string, review: string }) => {
   // const [starsCount, setStarsCount] = useState();
